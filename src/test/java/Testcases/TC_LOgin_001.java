0add10ea719ae9df85bfd04976a2609174b6e6ba
jsp <jsp@DESKTOP-1S6H73A>
2024-01-08 11:24:03 +0530

    "Token Code"

--- src/test/java/Testcases/TC_LOgin_001.java
+++ src/test/java/Testcases/TC_LOgin_001.java
@@ -2,4 +2,8 @@ package Testcases;
 
 public class TC_LOgin_001 {
 
+	public static void main(String[] args) {
+		System.out.println("https://github.com/bondmnk/DENO_M5.git");
+		System.out.println("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");
+	}
 }

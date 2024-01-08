738e7186eb4a5b9f8e18d423d50a724a725c8711
jsp <jsp@DESKTOP-1S6H73A>
2024-01-08 11:53:41 +0530

    "last"

--- src/test/java/Testcases/TC_LOgin_001.java
+++ src/test/java/Testcases/TC_LOgin_001.java
@@ -4,6 +4,6 @@ public class TC_LOgin_001 {
 
 	public static void main(String[] args) {
 		System.out.println("https://github.com/bondmnk/DENO_M5.git");
-		System.out.println("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");
+		System.out.println("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");
 	}
 }

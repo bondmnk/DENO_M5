af3f022f79503c9109a4481032697b1379af4efe
bondmnk <115972345+bondmnk@users.noreply.github.com>
2024-01-10 12:02:47 +0530

    Delete src/test/java/Testcases/TC_LOgin_001.java

--- src/test/java/Testcases/TC_LOgin_001.java
+++
@@ -1,9 +0,0 @@
-package Testcases;
-
-public class TC_LOgin_001 {
-
-	public static void main(String[] args) {
-		System.out.println("https://github.com/bondmnk/DENO_M5.git");
-		System.out.println("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");
-	}
-}
